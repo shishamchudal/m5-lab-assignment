19a984ab065e1832129fd17122096b5a44c157d7
Shisham Chudal <shishamchudal61@gmail.com>
2025-09-04 15:19:10 -0700

    Completed shopping cart

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [
+        {
+          image: "./products/cologne.jpg",
+          desc: "Unisex Cologne",
+          value: 0,
+        },
+        {
+          image: "./products/iwatch.jpg",
+          desc: "Apple iWatch",
+          value: 0,
+        },
+        {
+          image: "./products/mug.jpg",
+          desc: "Unique Mug",
+          value: 0,
+        },
+        {
+          image: "./products/wallet.jpg",
+          desc: "Mens Wallet",
+          value: 0,
+        },
+      ],
+    };
+  }
+
+  getTotalItems = () => {
+    return this.state.products.reduce((sum, product) => sum + product.value, 0);
+  };
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="bg-info">
+          <div className="container">
+            <div className="row py-5 mb-4">
+              <h1 className="col-md-6 mb-4">Shop to React</h1>
+              <div className="col-md-6 text-end mb-4">
+                <FontAwesomeIcon icon={faShoppingCart} /> {this.getTotalItems()}{" "}
+                items
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          {this.state.products.map((product) => (
+            <div key={product.id}>
+              <div>
+                {product.desc}
+                <br></br>
+                <div className="d-flex align-items-center">
+                  <img
+                    src={product.image}
+                    alt={product.desc}
+                    className="me-3"
+                    style={{ width: "100px" }}
+                  />
+
+                  <div className="d-flex align-items-center">
+                    <input
+                      type="number"
+                      min="0"
+                      value={product.value}
+                      onChange={(e) => {
+                        const value = parseInt(e.target.value, 10);
+                        this.setState((prevState) => ({
+                          products: prevState.products.map((p) =>
+                            p.desc === product.desc ? { ...p, value } : p
+                          ),
+                        }));
+                      }}
+                      className="form-control me-2"
+                      style={{ width: "60px" }}
+                    />
+                    <span>quantity</span>
+                  </div>
+                </div>
+              </div>
+              <hr></hr>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
